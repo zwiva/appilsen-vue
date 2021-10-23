e7137ec423333ebdcf2b7355779f49e5c549e8c4
Andrea Castro <ap.castrocastellon@gmail.com>
2021-10-23 01:55:19 -0300

    inicio estructura tienda

---
+++ src/components/auth/shop/ShopCart.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="prueba" style="display: flex; flex-direction: column ;place-items: center; height: 50vh">
+
+    <h2>Carrito Cervecero</h2>
+    <v-card class="ma-2 pa-3" width="80%">
+      <h4>Aca va el carro de compras -> sera tabla resumen</h4>
+      <table>
+        <img src="" alt="" />
+        <p>price</p>
+      </table>
+      <v-col cols="12">
+        <p>ancho completo</p>
+      </v-col>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "shopcart",
+};
+</script>
+
+<style>
+</style>
