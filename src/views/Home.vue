99900527221d428b09db484f865a5655f44d2447
Andrea Castro <ap.castrocastellon@gmail.com>
2021-10-25 03:38:18 -0300

    inicio home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,22 @@
 <template>
-  <h1>este es el home</h1>
+  <div>
+    <h1>este es el home</h1>
+    <v-row>
+      <img
+        src="https://www.collinsdictionary.com/images/thumb/beer_123784216_250.jpg?version=4.0.188"
+        alt=""
+        width="33%"
+      /><img
+        src="https://www.collinsdictionary.com/images/thumb/beer_123784216_250.jpg?version=4.0.188"
+        alt=""
+        width="33%"
+      /><img
+        src="https://www.collinsdictionary.com/images/thumb/beer_123784216_250.jpg?version=4.0.188"
+        alt=""
+        width="33%"
+      />
+    </v-row>
+  </div>
 </template>
 
 <script>
