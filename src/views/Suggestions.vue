9dcbb6582d12272c3af055f5392c98d6e0b0339d
Andrea Castro <ap.castrocastellon@gmail.com>
2021-10-25 02:50:02 -0300

    base sugerencias crear producto

---
+++ src/views/Suggestions.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div>
+      <h1>Recomendamos / Sugerimos / Inspirate:</h1>
+      <p>Inspirate con nuestras creaciones / ingredientes / combinaciones:</p>
+      <div v-for="(ourbeer, index) in internalBeers" :key="index">
+        <InternalCardBeer :ourbeer="ourbeer" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import InternalCardBeer from "../components/auth/InternalCardBeer.vue";
+export default {
+  name: "suggestions",
+  components: { InternalCardBeer },
+  data: () => ({
+    internalBeers: [
+      { name: "primerita", image: "", price: 2000 },
+      { name: "secunda", image: "", price: 2000 },
+      { name: "tercia", image: "", price: 2000 },
+    ],
+  }),
+};
+</script>
