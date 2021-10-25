eeea6090767e2aab519594cfd3308de6618725b6
Andrea Castro <ap.castrocastellon@gmail.com>
2021-10-25 03:04:44 -0300

    actualiza footer en shop

--- src/components/shared/Footer.vue
+++ src/components/shared/Footer.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< Updated upstream
   <div>
     <v-footer dark padless>
       <v-card class="flex" wrap flat tile>
@@ -26,55 +25,6 @@
       </v-card>
     </v-footer>
   </div>
-=======
-<div>
-  <v-container>    
-  <v-footer 
-   dark
-    padless
-  >
-    <v-card
-      class="flex"
-      flat
-      tile>
-      <v-card-title>
-        <strong class="subheading" color="amber">Conectate con nosotros en nuestras redes sociales!</strong>
-
-        <v-spacer></v-spacer>
-
-          <v-btn x-large color="primary" dark
-          ><i class="fab fa-google-plus-g fa-2x"> </i>
-          
-        </v-btn>
-         <v-btn x-large color="primary" dark
-          ><i class="fab fa-instagram fa-2x"></i> 
-          
-        </v-btn>
-
-      <!--   <v-btn 
-        class="icons mx-4"
-          v-for="icon in icons"
-          :key="icon"
-          icon
-        >
-           <v-icon 
-          class="icons"
-          color="primary"
-          size="24px"
-          dark>
-             {{ icon }} 
-          </v-icon>
-        </v-btn> --> 
-      </v-card-title>
-
-      <v-card-text class="py-2 white--text text-center">
-        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-      </v-card-text>
-    </v-card>
-  </v-footer>
-  </v-container>
-</div>
->>>>>>> Stashed changes
 </template>
 
 <script>
