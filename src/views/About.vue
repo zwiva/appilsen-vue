9bb95a6d2746dc9b4deb0408ae757e9d7b33cae9
Andrea Castro <ap.castrocastellon@gmail.com>
2021-11-04 22:14:18 -0300

    aplica v-cols en about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,28 +1,33 @@
 <template>
   <div>
     <v-container class="hero">
-      <h1 class="font-weight-bold text-left">Quienes somos</h1>
-
-      <br />
-
-      <v-row>
-        <div>
-          <p class="text-left my-2">
+      <div class="mx-6">
+        <v-col class="text-left" cols="12" sm="12" md="8">
+          <h1 class="font-weight-bold">Quienes somos</h1>
+          <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, quos
             magnam? Vitae nihil vel voluptatum delectus temporibus voluptatibus,
-            alias illum eaque ducimus laudantium fugiat ut fuga <br />excepturi
+            alias illum eaque ducimus laudantium fugiat ut fuga excepturi
             tenetur repellendus velit. Lorem ipsum dolor sit, amet consectetur
             adipisicing elit. Eos, quos magnam? Vitae nihil vel voluptatum
-            delectus temporibus <br />
-            voluptatibus, alias illum eaque ducimus laudantium fugiat ut fuga
-            excepturi tenetur repellendus velit.
+            delectus temporibus voluptatibus, alias illum eaque ducimus
+            laudantium fugiat ut fuga excepturi tenetur repellendus velit.
           </p>
-        </div>
-
-        <br />
-
-        <div class="player embed responsive">
+        </v-col>
+        <v-col cols="12" sm="12" md="11" lg="6" xl="6">
           <iframe
+            height="400px"
+            width="100%"
+            max-width="700px"
+            src="https://www.youtube.com/embed/guHhryViFxY"
+            title="YouTube video player"
+            frameborder="2"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          >
+          </iframe>
+
+          <!-- <iframe
             width="700"
             height="490"
             src="https://www.youtube.com/embed/guHhryViFxY"
@@ -30,38 +35,31 @@
             frameborder="2"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
-          ></iframe>
-          <br />
-        </div>
-
-        <br />
-        <div>
-          <p class="text-left my-2">
+          ></iframe> -->
+        </v-col>
+        <v-col class="text-left" cols="12" sm="12" md="6">
+          <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, quos
             magnam? Vitae nihil vel voluptatum delectus temporibus voluptatibus,
-            <br />
             alias illum eaque ducimus laudantium fugiat ut fuga excepturi
-            tenetur repellendus velit. Lorem ipsum dolor sit, amet consectetur
-            adipisicing elit. <br />
-            Eos, quos magnam? Vitae nihil vel voluptatum delectus temporibus
-            voluptatibus, alias illum eaque ducimus laudantium fugiat ut fuga
-            <br />
-            excepturi tenetur repellendus velit.
+            tenetur repellendus velit.
           </p>
-        </div>
-      </v-row>
-
-      <br />
-      <br />
-
-      <div>
-        <v-img
-        class="imgbtn"
-          src="../assets/imgbeers/logoAppilsen.jpg"
-          height="250px"
-          width="250px"
-          ><router-link :to="{ name: 'Home' }"> </router-link>
-        </v-img>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, quos
+            magnam? Vitae nihil vel voluptatum delectus temporibus voluptatibus,
+            alias illum eaque ducimus laudantium fugiat ut fuga excepturi
+            tenetur repellendus velit.
+          </p>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-img
+            class="mx-auto"
+            src="../assets/imgbeers/logoAppilsen.jpg"
+            height="250px"
+            width="250px"
+            ><router-link :to="{ name: 'Home' }"> </router-link>
+          </v-img>
+        </v-col>
       </div>
     </v-container>
   </div>
@@ -80,34 +78,9 @@ export default {
 .hero {
   background-image: url("../assets/imgbeers/bg-beer4.jpg");
   background-repeat: no-repeat;
-  background-size: cover;
+  /* background-size: cover; */
   background-position: left;
   color: white;
-  padding-bottom: 10rem;
+  /* padding-bottom: 10rem; */
 }
-
-.player {
-  float: left;
-  position: relative;
-  margin-left: 5rem;
-  margin-top: 3rem;
-  overflow: hidden;
-}
-
-.imgbtn {
-  justify-content: center;
-  margin-left: 20rem;
-  margin-top: 1rem;
-  
-}
-
-h1 {
-  margin-left: 5rem;
-}
-
-p {
-  margin-left: 5rem;
-}
-
-
 </style>
