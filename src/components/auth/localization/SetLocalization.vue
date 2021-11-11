9324e412c152b457d96ac9d6291c8865bd7e88e6
Andrea Castro <ap.castrocastellon@gmail.com>
2021-11-11 04:51:34 -0300

    actualizacion

--- src/components/auth/localization/SetLocalization.vue
+++ src/components/auth/localization/SetLocalization.vue
@@ -5,88 +5,110 @@
       <div class="ma-3">
         <h4 class="py-3">¿Qué quieres hacer con tu pedido?</h4>
         <v-row class="pa-1">
-          <!-- botonera seleccion destino -->
+          <!-- ESCOGE RETIRAR -->
           <v-col cols="12" sm="4" class="text-center">
             <v-btn small @click="selectPickup">
               Retirar<v-icon small>mdi-shopping</v-icon>
             </v-btn>
-          </v-col>
-          <v-col cols="12" sm="4" class="text-center">
-            <v-btn small @click="selectStore">
-              Consumir<v-icon small>mdi-glass-mug-variant</v-icon>
-            </v-btn>
-          </v-col>
-          <v-col cols="12" sm="4" class="text-center">
-            <v-btn small @click="selectDispatch">
-              Despacho<v-icon small>mdi-truck-outline</v-icon>
-            </v-btn>
-          </v-col>
-
-          <!-- selectores post seleccion destino -->
-          <div class="mx-auto">
-            <v-select
-              label="Escoge ubicación"
-              :rules="rules.selectedField"
-              v-model="destination.shop"
-              :items="shops"
-              v-if="pickup"
-              @change="setDestinationPickUp(destination.shop)"
-            >
-            </v-select>
-            <v-select
-              label="Escoge tienda"
-              :rules="rules.selectedField"
-              v-model="destination.shop"
-              :items="shops"
-              v-if="store"
-              @change="setDestinationShop()"
-            >
-            </v-select>
-
-            <v-row class="justify-center align-center">
-              <v-text-field
-                label="Ingresa una dirección"
-                :rules="rules.completedField"
-                v-model="destination.address"
-                v-if="dispatch"
-                class="px-6"
+            <div v-if="pickup">
+              <v-select
+                label="Escoge ubicación"
+                :rules="rules.selectedField"
+                v-model="destination.shop"
+                :items="shops"
+                @change="setDestination(destination.shop)"
               >
-              </v-text-field>
+              </v-select>
+              <p>{{ destination.shop }}</p>
               <v-btn
+                v-if="dispatch"
                 class="mx-1"
                 small
-                v-if="dispatch"
                 :disabled="loading"
                 :class="{ disabled: loading }"
                 @click="getLocation"
               >
                 Obtener dirección
               </v-btn>
-            </v-row>
-            <div class="form-group text-center">
+            </div>
+          </v-col>
+
+          <!-- ESCOGE CONSUMIR -->
+          <v-col cols="12" sm="4" class="text-center">
+            <v-btn small @click="selectStore">
+              Consumir<v-icon small>mdi-glass-mug-variant</v-icon>
+            </v-btn>
+            <div v-if="store">
+              <v-select
+                label="Escoge tienda"
+                :rules="rules.selectedField"
+                v-model="destination.shop"
+                :items="shops"
+                @change="setDestination(destination.shop)"
+              >
+              </v-select>
+              <p>{{ destination.shop }}</p>
+
+              <v-btn
+                v-if="dispatch"
+                class="mx-1"
+                small
+                :disabled="loading"
+                :class="{ disabled: loading }"
+                @click="getLocation"
+              >
+                Obtener dirección
+              </v-btn>
+            </div>
+          </v-col>
+
+          <!-- ESCOGE DESPACHO -->
+          <v-col cols="12" sm="4" class="text-center">
+            <v-btn small @click="selectDispatch">
+              Despacho<v-icon small>mdi-truck-outline</v-icon>
+            </v-btn>
+            <div v-if="dispatch">
+              <v-row class="justify-center align-center">
+                <v-text-field
+                  label="Ingresa una dirección"
+                  :rules="rules.completedField"
+                  v-model="destination.address"
+                  class="px-6"
+                >
+                </v-text-field>
+                <v-btn
+                  class="mx-1"
+                  small
+                  v-if="dispatch"
+                  :disabled="loading"
+                  :class="{ disabled: loading }"
+                  @click="getLocation"
+                >
+                  Obtener dirección
+                </v-btn>
+              </v-row>
               <input
                 type="text"
                 class="location-control"
                 :value="location"
                 readonly
               />
-              <!-- <button
-                              type="button"
-                              class="copy-btn"
-                              @click="copyLocation"
-                            >
-                              copiar
-                            </button> -->
+
+              <v-btn type="button" class="copy-btn" @click="copyLocation">
+                Copiar direccion
+              </v-btn>
             </div>
-          </div>
+          </v-col>
         </v-row>
+
         <v-spacer></v-spacer>
+
         <!-- MAPA !!! -->
         <div>
           <div class="main">
             <div class="flex pa-3 ma-3">
               <!-- Map -->
-              <p>Mapa</p>
+              <v-btn @click="showMap()">Mapa</v-btn>
               <div class="map-holder">
                 <div id="map"></div>
               </div>
@@ -124,6 +146,7 @@ export default {
     pickup: false,
     store: false,
     dispatch: false,
+
     rules: {
       completedField: [
         (val) => (val || "").length > 0 || "Por favor completar.",
@@ -164,47 +187,27 @@ export default {
       this.store = false;
       this.dispatch = false;
     },
-    setDestinationPickUp(shop) {
+    setDestination(shop) {
       switch (shop) {
         case this.shops[0]:
-          console.log("escogio ", this.shops[0]);
           this.center = [this.lats[0], this.longs[0]];
-          // this.latitude = ;
-          // this.longitude = ;
-          console.log("center", this.center);
-          this.createMap(); // COMENTADO PARA EVITAR HACER TODAS LAS PETICIONES
-
-          // console.log("longitud ", this.longitude);
+          
           break;
         case this.shops[1]:
-          this.latitude = this.lats[1];
-          this.longitude = this.longs[1];
-          console.log("escogio ", this.shops[1]);
-          console.log("latitud ", this.latitude);
-          console.log("longitud ", this.longitude);
-          this.createMap(); // COMENTADO PARA EVITAR HACER TODAS LAS PETICIONES
+          this.center = [this.lats[1], this.longs[1]];
 
           break;
         case this.shops[2]:
-          this.latitude = this.lats[2];
-          this.longitude = this.longs[2];
-          console.log("escogio ", this.shops[2]);
-          console.log("latitud ", this.latitude);
-          console.log("longitud ", this.longitude);
+          this.center = [this.lats[2], this.longs[2]];
+
           break;
         case this.shops[3]:
-          this.latitude = this.lats[3];
-          this.longitude = this.longs[3];
-          console.log("escogio ", this.shops[3]);
-          console.log("latitud ", this.latitude);
-          console.log("longitud ", this.longitude);
+          this.center = [this.lats[3], this.longs[3]];
+
           break;
         case this.shops[4]:
-          this.latitude = this.lats[4];
-          this.longitude = this.longs[4];
-          console.log("escogio ", this.shops[4]);
-          console.log("latitud ", this.latitude);
-          console.log("longitud ", this.longitude);
+          this.center = [this.lats[4], this.longs[4]];
+
           break;
         default:
           break;
@@ -231,15 +234,19 @@ export default {
       this.showOrder = true;
       console.log("meter en la store");
     },
+    showMap() {
+      this.createMap(); // COMENTADO PARA EVITAR HACER TODAS LAS PETICIONES
+    },
     // mapa:
     async createMap() {
       try {
         mapboxgl.accessToken = this.access_token;
+
         this.map = new mapboxgl.Map({
           container: "map",
           style: "mapbox://styles/mapbox/streets-v11",
           center: this.center,
-          zoom: 11,
+          zoom: 16,
         });
 
         let geocoder = new MapboxGeocoder({
@@ -258,6 +265,7 @@ export default {
             .setLngLat(e.result.center)
             .addTo(this.map);
           this.center = e.result.center;
+          console.log("center", this.center);
           marker.on("dragend", (e) => {
             this.center = Object.values(e.target.getLngLat());
           });
@@ -275,7 +283,6 @@ export default {
         const response = await axios.get(
           `https://api.mapbox.com/geocoding/v5/mapbox.places/${this.center[0]},${this.center[1]}.json?access_token=${this.access_token}`
         );
-        console.log("response", response);
         this.loading = false;
         this.location = response.data.features[0].place_name;
         console.log("location", this.location);
@@ -284,6 +291,7 @@ export default {
         console.log(err);
       }
     },
+
     copyLocation() {
       if (this.location) {
         navigator.clipboard.writeText(this.location);
@@ -295,6 +303,3 @@ export default {
   props: {},
 };
 </script>
-
-<style>
-</style>
